5f834aeead316d3895ac6a51aa169f1e63d13101
TomAllenBerg <tomberg@mtu.edu>
2021-10-20 19:12:44 -0400

    Update stylying for login page, add components

--- frontend/src/components/LoginCard.vue
+++ frontend/src/components/LoginCard.vue
@@ -1,58 +1,69 @@
 <template>
 <div>
-  <div class="center">
+  <!-- <div class="center"> -->
    <div class="login-form">
     <div id="disabledWrapper">
-     <form id="loginForm" action="" method="post">
-     <b-form-group has-error>
-       <img alt="myHealthBin logo" src="../assets/myHealthBinLogo.png" />
-       <br>
-       <h2 class="text-center">Welcome to MyHealthBin!</h2>
-       <h4 class="text-center">Login to get started</h4>
-       <br>
-       <label for="Username"><strong>Username</strong></label>
-        <b-form-input v-model="text" placeholder="Username" required="required"></b-form-input>
-       <div class="invalid-feedback">
-       </div>
-     </b-form-group>
+      <div class="loginColorWrapper">
+       <form id="loginForm" action="" method="post">
+       <b-form-group has-error>
+         <h3>Login or <b>Sign-up</b></h3>
+         <label for="Username"><strong>Username</strong></label>
+         <Input />
+         <div class="invalid-feedback">
+         </div>
+       </b-form-group>
 
-        <div class="form-group has-error">
-          <label for="Password"><strong>Password</strong></label>
-          <b-form-input v-model="password" placeholder="Password" required="required"></b-form-input>
-            <div class="invalid-feedback">
-            </div>
-        </div>
-        <b-form-group has-error>
+          <b-form-group has-error>
+            <label for="Password"><strong>Password</strong></label>
+            <Input />
+              <div class="invalid-feedback">
+              </div>
+          </b-form-group>
+          
+          <b-form-group has-error>
 
-        </b-form-group>
+          </b-form-group>
 
-        <b-form-group>
-          <b-button variant="primary btn-block" value="login">Log in</b-button>
-        </b-form-group>
-    </form>
+          <ButtonBlock />
+
+      </form>
+      </div>
     </div>
    </div>
-  </div>
+  <!-- </div> -->
 </div>
 </template>
 
 <script>
+// @ is an alias to /src
+import ButtonBlock from '@/components/ButtonBlock.vue';
+import Input from '@/components/Input.vue';
+
 export default {
-  name: "LoginCard"
+  name: "LoginCard",
+  components: {
+    ButtonBlock,
+    Input
+  },
 };
 </script>
 
 <style lang="scss" scoped>
-/*
-This file is the style sheet for the login page.
-@author Tom Berg
-@Date Last Modified: 1/23/2020
-*/
+h3 {
+  font-size: 1.9rem;
+}
+
+.loginColorWrapper {
+  background: #d2fdff;
+  padding: 1.5em;
+}
 
 .login-form {
 		width: 340px;
-    	margin: 50px auto;
+    margin: 3em auto;
 	}
+
+
 
 	/* Align MTU logo to center */
 	.login-form img {
@@ -66,8 +77,7 @@ This file is the style sheet for the login page.
 		float: center;
 	}
     .login-form form {
-    	margin-bottom: 15px;
-        background: #f7f7f7;
+        background-color: #b4dfe5;
         box-shadow: 0px 2px 2px rgba(0, 0, 0, 0.3);
         padding: 30px;
     }
