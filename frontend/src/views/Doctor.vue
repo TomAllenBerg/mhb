de9b314b7254dfbfc9073497f7f25c646f9a9201
Tom Berg <tomberg@mtu.edu>
2021-10-21 17:21:17 -0400

    Delete frontend directory
    No longer need the frontend directory seeing as it is duplicated when project is set up using php laravel.

--- frontend/src/views/Doctor.vue
+++
@@ -1,5 +0,0 @@
-<template>
-  <div class="doctor">
-    <h1>This is the doctor page</h1>
-  </div>
-</template>
--- frontend/src/views/Prescription.vue
+++
@@ -1,5 +0,0 @@
-<template>
-  <div class="prescription">
-    <h1>This is the prescription page</h1>
-  </div>
-</template>
