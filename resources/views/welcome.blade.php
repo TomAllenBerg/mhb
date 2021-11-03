e4d8d061ca7474aef6078a58ddde5c144c7cde6b
Noah Deverney <nddevern@mtu.edu>
2021-11-03 13:23:25 -0400

    Fixed favicon not working on Chrome hopefully

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -5,6 +5,7 @@
         <meta name="viewport" content="width=device-width, initial-scale=1">
 
         <title>{{ config('app.name', 'MyHealthBin') }}</title>
+        <link rel="icon" type="image/ico" href="/images/favicon.ico"/>
 
         <!-- Fonts -->
         <link href="https://fonts.googleapis.com/css2?family=Nunito:wght@400;600;700&display=swap" rel="stylesheet">
