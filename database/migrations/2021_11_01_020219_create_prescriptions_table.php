374290a37e7e616ed8665f75ede2d3b605c4c8d9
Benjamin Collicott <55672347+BCollicott@users.noreply.github.com>
2021-11-12 13:51:15 -0500

    Updated prescription migration schema

--- database/migrations/2021_11_01_020219_create_prescriptions_table.php
+++ database/migrations/2021_11_01_020219_create_prescriptions_table.php
@@ -16,11 +16,14 @@ class CreatePrescriptionsTable extends Migration
         // Create schema for prescriptions
         Schema::create('prescriptions', function (Blueprint $table) {
             $table->id();
-            $table->string('p_name');
-            //$table->string('name');
-            //$table->integer('strength');
+            $table->string('name');
             $table->integer('dosage');
-            //$table->string('side_effects');
+            $table->float('dosage_amt', 5, 2);
+            $table->enum('dosage_unit', ['g', 'mg', 'kg', 'L', 'mL', 'tablets', 'drops']);
+            // From https://www.merckmanuals.com/home/drugs/administration-and-kinetics-of-drugs/drug-administration
+            $table->enum('route', ['oral', 'subcutaneous injection', 'intramuscular injection', 'intravenous injection', 'inhalation', 'ocular', 'topical']);
+            $table->string('frequency');
+            $table->string('side_effects');
             $table->timestamps();
         });
 
