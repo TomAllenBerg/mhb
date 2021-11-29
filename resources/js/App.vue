693db65ea078ed1b9d4468f8029f79e694a50df2
Noah Deverney <nddevern@mtu.edu>
2021-11-29 11:25:01 -0500

    Designed and fleshed out the dashboard/home page. It contains links to other pages.
    I assume we are planning on removing the top navigation bar and having our app only use the left sidebar and navigation from this dashboard. I figured this would be a good landing page for people. Let me know your thoughts on this/the navigational design of the website as a whole if you have any other ideas.
    Oh I also added the doctors page to the top nav bar for consistency even though it will eventually be removed later.

--- resources/js/App.vue
+++ resources/js/App.vue
@@ -5,6 +5,7 @@
           <router-link to="/about">About</router-link> |
           <router-link to="/prescription">Prescriptions</router-link> |
           <router-link to="/medical">Medical History</router-link> |
+          <router-link to="/doctor">Doctors</router-link> |
           <router-link to="/login">Login</router-link> |
           <router-link to="/register">Register</router-link>
       </div>
--- resources/js/views/Home.vue
+++ resources/js/views/Home.vue
@@ -1,8 +1,48 @@
 <template>
   <div class="home">
     <Sidebar/>
-    <img alt="MyHealthBin Logo" src="../assets/fav.png">
-    <h1>Welcome to MyHealthBin!</h1>
+    <div class="container">
+      <div class="topGrid">
+        <div class="topGridItem" style="justify-self: start;">
+          <img alt="MyHealthBin Logo" src="../assets/fav.png" style="width: 50%; height: auto; justify-self: end;">
+        </div>
+        <div class="topGridItem" style="justify-self:end;">
+          <h1 style="font-size:45px;">Welcome to MyHealthBin!</h1>
+          <p><router-link to="/login" id="LoginRegisterLink">Login</router-link> | 
+          <router-link to="/register" id="LoginRegisterLink">Register</router-link></p>
+        </div>
+      </div>
+      <div class="grid">
+        <button class="gridItemButton" @click="$router.push('about')">
+        <div class="gridItem">
+          <img src="../assets/sidebarLogos/AboutIcon.png" id="smallIcon">
+          <h1 id="sectionHeading">About</h1>
+          <p>Learn more about our mission and who we are.</p>
+        </div>
+        </button>
+        <button class="gridItemButton" @click="$router.push('prescription')">
+        <div class="gridItem">
+          <img src="../assets/sidebarLogos/PrescriptionIcon.png" id="smallIcon">
+          <h1 id="sectionHeading">Prescriptions</h1>
+          <p>View & add prescriptions.</p>
+        </div>
+        </button>
+        <button class="gridItemButton" @click="$router.push('medical')">
+        <div class="gridItem">
+          <img src="../assets/sidebarLogos/HistoryIcon.png" id="smallIcon">
+          <h1 id="sectionHeading">Medical History</h1>
+          <p>View & add to your medical history.</p>
+        </div>
+        </button>
+        <button class="gridItemButton" @click="$router.push('doctor')">
+        <div class="gridItem">
+          <img src="../assets/sidebarLogos/DoctorIcon.png" id="smallIcon">
+          <h1 id="sectionHeading">Doctors</h1>
+          <p>View your doctors.</p>
+        </div>
+        </button>
+      </div>
+    </div>
 
   </div>
 </template>
@@ -18,8 +58,56 @@ export default {
 }
 </script>
 
-<style>
-.home {
-  color: white;
-}
+<style land="scss" scoped>
+  .home {
+    /*color: white;*/
+    padding-left: 130px;
+  }
+  #sectionHeading {
+    display: inline;
+  }
+  #smallIcon {
+    height: 25px;
+    width: auto;
+  }
+  .topGrid {
+    display: grid;
+    grid-template-rows: 1fr;
+    grid-template-columns: 1fr 1fr;
+    gap: 60px;
+    margin-bottom: 60px;
+  }
+  .container {
+    margin-left: auto;
+    margin-right: auto;
+    justify-content: center;
+    padding: 20px;
+    min-width: 650px;
+    max-width: 900px
+  }
+  .grid {
+    display: grid;
+    justify-content: space-between;
+    align-content: stretch;
+    gap: 30px;
+    grid-template-rows: 1fr 1fr;
+    grid-template-columns: 1fr 1fr;
+  }
+  .topGridItem{
+    align-self: center;
+  }
+  .gridItem{
+    background: #d2fdff;
+    color: #303c6c;
+    margin: 5px;
+  }
+  .gridItemButton {
+    background: #b4dfe5;
+    border: none;
+    cursor: pointer;
+    /*min-width: 300px;*/
+  }
+  #LoginRegisterLink {
+    color: #FFFFFF
+  }
 </style>
